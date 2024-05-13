10aaac3298877969b79e90269589d00784963a34
Leonardo_MD <143217207+LeoEmiDe@users.noreply.github.com>
2024-05-13 14:56:12 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorDeEstudantesT7
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Form_Load(object sender, EventArgs e)
+        {                         
+        }
+
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            // Cria um objeto da classe "MeuBancoDedados".
+            MeuBancoDeDados meuBancoDeDados = new MeuBancoDeDados();
+
+            MySqlDataAdapter meuAdaptadorSql = new MySqlDataAdapter();
+            DataTable tabelaDeDados = new DataTable();
+            MySqlCommand comandoSql =  new MySqlCommand("SELECT * FROM `usuarios` WHERE `nome_de_usuario`= @usuario AND `senha` = @senha", meuBancoDeDados.getConexao );
+
+            comandoSql.Parameters.Add("usuario", MySqlDbType.VarChar).Value = textBoxUsuario.Text;
+            comandoSql.Parameters.Add("senha", MySqlDbType.VarChar).Value = textBoxSenha.Text;
+
+            meuAdaptadorSql.SelectCommand = comandoSql;
+
+            meuAdaptadorSql.Fill(tabelaDeDados);
+            
+            if (tabelaDeDados.Rows.Count > 0)
+            {
+                MessageBox.Show("ACESSO AUTORIZADO");
+            }
+            else
+            {
+                MessageBox.Show("Usuário e(ou) senha incorreto(s)","Erro de Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonCancelar_Click(object sender, EventArgs e)
+        {
+            Close ();
+        }
+    }
+}
